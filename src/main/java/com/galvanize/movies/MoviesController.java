fee35360bbe961b66e93340a8202c194a14c4ba4
EdgarBarrientos <Edgar_O_Barrientos@outlook.com>
2021-05-25 21:26:42 -0400

    Add second test

--- src/main/java/com/galvanize/movies/MoviesController.java
+++ src/main/java/com/galvanize/movies/MoviesController.java
@@ -1,9 +1,11 @@
 package com.galvanize.movies;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -18,7 +20,7 @@ public class MoviesController {
         Root root = new Root();
         root.title = "The Godfather";
         root.minutes= 175;
-        root.genre = "Crime, Drama";
+        root.genre = null;
         root.rating = 9.2;
         root.metascore = 100;
         root.description = "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.";
@@ -27,7 +29,7 @@ public class MoviesController {
         root.year = "1972";
 
         Person myperson = new Person();
-        myperson.firstName = "Francis Ford";
+        myperson.firstName =null;
         myperson.lastName = "Copolla";
         myperson.role = "Director";
 
@@ -72,41 +74,140 @@ public class MoviesController {
 
     }
 
+    @PostMapping("/gross/total")
+    public int getJSONData(@RequestBody ArrayList<Root> roots) {
+        int gross=0;
+        for (Root item: roots)
+         {
+             gross+= item.gross;
+        }
+        return gross;
+    }
+
+    @JsonNaming(PropertyNamingStrategy.UpperCamelCaseStrategy.class)
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     public static class Person{
-        @JsonProperty("Role")
+
         public String role;
-        @JsonProperty("FirstName")
+
         public String firstName;
-        @JsonProperty("LastName")
+
         public String lastName;
     }
 
+    @JsonNaming(PropertyNamingStrategy.UpperCamelCaseStrategy.class)
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     public static class Credit{
-        @JsonProperty("Person")
+
         public Person person;
     }
 
+    @JsonNaming(PropertyNamingStrategy.UpperCamelCaseStrategy.class)
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     public static class Root{
-        @JsonProperty("Title")
+
         public String title;
-        @JsonProperty("Minutes")
+
         public int minutes;
-        @JsonProperty("Genre")
+
         public String genre;
-        @JsonProperty("Rating")
+
         public double rating;
-        @JsonProperty("Metascore")
-        public int metascore;
-        @JsonProperty("Description")
+
+
         public String description;
-        @JsonProperty("Votes")
+
         public int votes;
-        @JsonProperty("Gross")
+
+
         public double gross;
-        @JsonProperty("Year")
+
         public String year;
-        @JsonProperty("Credits")
+
         public List<Credit> credits;
+
+        public int metascore;
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public int getMinutes() {
+            return minutes;
+        }
+
+        public void setMinutes(int minutes) {
+            this.minutes = minutes;
+        }
+
+        public String getGenre() {
+            return genre;
+        }
+
+        public void setGenre(String genre) {
+            this.genre = genre;
+        }
+
+        public double getRating() {
+            return rating;
+        }
+
+        public void setRating(double rating) {
+            this.rating = rating;
+        }
+
+        public int getMetascore() {
+            return metascore;
+        }
+
+        public void setMetascore(int metascore) {
+            this.metascore = metascore;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public int getVotes() {
+            return votes;
+        }
+
+        public void setVotes(int votes) {
+            this.votes = votes;
+        }
+
+        public double getGross() {
+            return gross;
+        }
+
+        public void setGross(double gross) {
+            this.gross = gross;
+        }
+
+        public String getYear() {
+            return year;
+        }
+
+        public void setYear(String year) {
+            this.year = year;
+        }
+
+        public List<Credit> getCredits() {
+            return credits;
+        }
+
+        public void setCredits(List<Credit> credits) {
+            this.credits = credits;
+        }
+
+
     }
 
 }
--- src/test/java/com/galvanize/movies/MoviesControllerTest.java
+++ src/test/java/com/galvanize/movies/MoviesControllerTest.java
@@ -1,32 +1,59 @@
 package com.galvanize.movies;
-
+import com.jayway.jsonpath.PathNotFoundException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.hamcrest.Matchers.is;
-
-
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(MoviesController.class)
 public class MoviesControllerTest {
-
     @Autowired
     private MockMvc mvc;
-
     @Test
-    public void testMovies() throws Exception{
+    void testMovies() throws Exception{
         RequestBuilder request = get("/movies/movie");
-
         this.mvc.perform(request)
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.Title", is("The Godfather")))
                 .andExpect(jsonPath("$.Gross", is(134.97)));
+    }
+    @Test
+    void testMoviesNoAttributeException() throws Exception{
+        RequestBuilder request = get("/movies/movie");
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                //.andExpect( assertThrows( PathNotFoundException.class, () -> {
+                .andExpect(
+                        jsonPath("$.Credits[0].Person.FirstName").doesNotHaveJsonPath() );
+    }
+  @Test
+    public void testRawBody() throws Exception {
+        String json = getJSON("C:\\Users\\913170\\IdeaProjects\\Movies\\src\\main\\resources\\movies.json");
 
+        MockHttpServletRequestBuilder request = post("/movies/gross/total")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
 
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string("191"));
+    }
+
+    private String getJSON(String path) throws Exception {
+        return new String(Files.readAllBytes(Paths.get(path)));
     }
 }
