0a7f506e1d6fadeda4194ba6d791476fea98eb03
Castillo <905903@cognizant.com>
2021-05-25 20:42:57 -0500

    updated tests

--- src/main/java/com/galvanize/movies/MoviesController.java
+++ src/main/java/com/galvanize/movies/MoviesController.java
@@ -1,77 +1,61 @@
 package com.galvanize.movies;
-
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
 @RestController
 @RequestMapping("/movies")
 public class MoviesController {
-
     @GetMapping("/movie")
     public Root buildOutputMovie(){
         Root root = new Root();
         root.title = "The Godfather";
         root.minutes= 175;
-        root.genre = "Crime, Drama";
+        root.genre = null;
         root.rating = 9.2;
         root.metascore = 100;
         root.description = "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.";
         root.votes = 1561591;
         root.gross = 134.97;
         root.year = "1972";
-
         Person myperson = new Person();
-        myperson.firstName = "Francis Ford";
+        //myperson.firstName = "Francis Ford";
         myperson.lastName = "Copolla";
         myperson.role = "Director";
-
         Person myperson2 = new Person();
         myperson2.firstName = "Marlon";
         myperson2.lastName = "Brando";
         myperson2.role = "Star";
-
         Person myperson3 = new Person();
         myperson3.firstName = "Al";
         myperson3.lastName = "Pacino";
         myperson3.role = "Star";
-
         Person myperson4 = new Person();
         myperson4.firstName = "James";
         myperson4.lastName = "Caan";
         myperson4.role = "Star";
-
         Person myperson5 = new Person();
         myperson5.firstName = "Diane";
         myperson5.lastName = "Keaton";
         myperson5.role = "Star";
-
-
         Credit credit1 = new Credit();
         credit1.person = myperson;
-
         Credit credit2 = new Credit();
         credit2.person = myperson2;
-
         Credit credit3 = new Credit();
         credit3.person = myperson3;
-
         Credit credit4 = new Credit();
         credit4.person = myperson4;
-
         Credit credit5 = new Credit();
         credit5.person = myperson5;
         root.credits = Arrays.asList(credit1,credit2,credit3, credit4, credit5);
-
         return root;
-
     }
-
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     public static class Person{
         @JsonProperty("Role")
         public String role;
@@ -80,12 +64,12 @@ public class MoviesController {
         @JsonProperty("LastName")
         public String lastName;
     }
-
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     public static class Credit{
         @JsonProperty("Person")
         public Person person;
     }
-
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     public static class Root{
         @JsonProperty("Title")
         public String title;
@@ -108,5 +92,4 @@ public class MoviesController {
         @JsonProperty("Credits")
         public List<Credit> credits;
     }
-
 }
--- src/test/java/com/galvanize/movies/MoviesControllerTest.java
+++ src/test/java/com/galvanize/movies/MoviesControllerTest.java
@@ -1,32 +1,61 @@
 package com.galvanize.movies;
-
+import com.jayway.jsonpath.PathNotFoundException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.hamcrest.Matchers.is;
-
-
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(MoviesController.class)
 public class MoviesControllerTest {
-
     @Autowired
     private MockMvc mvc;
-
     @Test
-    public void testMovies() throws Exception{
+    void testMovies() throws Exception{
         RequestBuilder request = get("/movies/movie");
-
         this.mvc.perform(request)
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.Title", is("The Godfather")))
                 .andExpect(jsonPath("$.Gross", is(134.97)));
+    }
+    @Test
+    void testMoviesNoAttributeException() throws Exception{
+        RequestBuilder request = get("/movies/movie");
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                //.andExpect( assertThrows( PathNotFoundException.class, () -> {
+                .andExpect(
+                        jsonPath("$.Credits[0].Person.FirstName").doesNotHaveJsonPath() );
+    }
+
+    @Test
+    void testGrossTotal() throws Exception {
+        String json = getJSON("/static/jsonData.json");
+        MockHttpServletRequestBuilder request = post("/movies/gross/total")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string("192.27"));
 
 
+    }
+
+    private String getJSON(String path) throws Exception {
+        URL url = new URL("file:///C:/Users/913170/IdeaProjects/SpringPlayground/build/resources/main/static/jsonData.json");
+        return new String(Files.readAllBytes(Paths.get("")));
     }
 }
